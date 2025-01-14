e61f9cf5675b840cb8c905b4c742051d7b9d8d0d
anqihuang <1079255145@qq.com>
2025-01-14 18:12:23 +0800

    完善文档说明并新增核心服务单元测试与杂记文件
    - 在文档的特定文件中补充了功能注释的相关内容，并添加了关键字分类部分。
    - 新建了核心服务的单元测试文件 `core.service.spec.ts` ，对 `CoreService` 进行了基本的定义和 `getHello` 方法的测试。
    - 新增了 `doc\杂记.txt` 文件。

---
+++ backend/test/unit/core/core.service.spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CoreService } from '../../../src/core/core.service';
+
+describe('CoreService', () => {
+  let service: CoreService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [CoreService],
+    }).compile();
+
+    service = module.get<CoreService>(CoreService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('getHello', () => {
+    it('should return "Hello World!"', () => {
+      expect(service.getHello()).toBe('Hello World!');
+    });
+  });
+});
